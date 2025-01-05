0c3672b4f6e6098b81e9815f3d2a7be182c9393c
masing4christ <masing4christ@gmail.com>
2025-01-05 09:52:18 +0000

    Update the project

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
 import './assets/scss/main.scss';
+
+const formEl = document.querySelector('#signup-form') as HTMLElement;
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const inputs = formEl.querySelectorAll('input') as NodeListOf<HTMLInputElement>
+    const errors = formEl.querySelectorAll('.error-message') as NodeListOf<HTMLElement>;
+    const errorMessages: string[] = [];
+
+    // Loop through all input elements
+    inputs.forEach((input) => {
+      let errorMessage = '';
+  
+      // Check if the input is invalid
+      if (!input.checkValidity()) {
+        if (input.value === '') {
+          // Custom message for empty inputs
+          errorMessage = `${input.placeholder} cannot be empty`;
+        } else if (input.type === 'email' && input.validity.typeMismatch) {
+          // Custom message for invalid email format
+          errorMessage = 'Looks like this is not an email';
+        } else {
+          // Use the default validation message for other cases
+          errorMessage = input.validationMessage;
+        }
+      }
+  
+      // Add the error message (or empty string if valid) to the array
+      errorMessages.push(errorMessage);
+    });
+
+    errors.forEach((error, index) => {
+        error.innerText = errorMessages[index];
+    })
+});
